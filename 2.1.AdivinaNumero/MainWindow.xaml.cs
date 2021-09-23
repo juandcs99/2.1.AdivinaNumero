288aca26e2db0f72465d55a6e26120b66d0772d8
Juandi <Juandi@DESKTOP-LVHFGRV>
2021-09-23 18:08:08 +0200

    cambios

--- 2.1.AdivinaNumero/MainWindow.xaml.cs
+++ 2.1.AdivinaNumero/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Media;
 
 namespace _2._1.AdivinaNumero
@@ -9,13 +10,35 @@ namespace _2._1.AdivinaNumero
         public MainWindow()
         {
             InitializeComponent();
+            Numero = GenerarAleatorio();
         }
 
-        
+        public int Numero { get; private set; }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private int GenerarAleatorio()
         {
+            Random rnd = new Random();
+            int numero = rnd.Next(0, 101);
+            return numero;
 
+        }
+
+        private void ComprobarButton_Click(object sender, RoutedEventArgs e, int numero)
+        {
+            if(int.Parse(numeroTextBox.Text) < Numero) pistaTextBlock.Text = "Te has quedado corto.";
+            
+            else if(int.Parse(numeroTextBox.Text) == Numero)
+            {
+                pistaTextBlock.Text = "¡HAS ACERTADO!";
+            }
+            else pistaTextBlock.Text = "Te has pasado.";
+        }
+
+        private void ReiniciarButton_Click(object sender, RoutedEventArgs e)
+        {
+            Numero = GenerarAleatorio();
+            pistaTextBlock.Text = "";
         }
     }
 }
+
